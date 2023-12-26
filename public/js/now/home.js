519245720039ef9f77a3044290caa3d2342ec313
masudranadeveloper <masud1010rana0101@gmail.com>
2023-12-26 21:25:59 +0600

    pok

--- public/js/now/home.js
+++ public/js/now/home.js
@@ -1,41 +1,41 @@
 /*JS isn't my expertise 😉*/
-$(document).ready(function() {
-    $("#menuButton").on("click", function(){
+$(document).ready(function () {
+    $("#menuButton").on("click", function () {
         $(".side-a").toggleClass("is-open");
         $("html").toggleClass("is-nav-open");
     });
-      $("#darkMode").on("click", function(){
+    $("#darkMode").on("click", function () {
         $("html").toggleClass("is-dark");
     });
 });
 
 // window
-$(window).click(function(){
+$(window).click(function () {
     $("#searchWrapper").css("display", "none");
 });
 
 // ajax
-$("#searchTitle").keyup(function(){
-    if($("#searchTitle").val() == ""){
+$("#searchTitle").keyup(function () {
+    if ($("#searchTitle").val() == "") {
         $("#searchWrapper").css("display", "none");
     }
 
     $.ajax({
-        "url" : urls.api_search,
-        "method" : "POST",
-        "data" : {
-            "title" : $("#searchTitle").val()
+        "url": api_search.api_search,
+        "method": "POST",
+        "data": {
+            "title": $("#searchTitle").val()
         },
-        success:function(data){
+        success: function (data) {
             $("#searchWrapper").css("display", "block");
-            if(data == "[]"){
+            if (data == "[]") {
                 return `
                 <div class="search">
                     <p>No data found!</p>
                 </div>
                 `
             }
-            const mapData = data.map((data)=>{
+            const mapData = data.map((data) => {
                 return `
                 <a href="${data.url}" class="search">
                     ${data.icons}
--- resources/views/users/layout/master.blade.php
+++ resources/views/users/layout/master.blade.php
@@ -154,7 +154,7 @@
         <!-- partial -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
-            const urls = {
+            const api_search = {
                 "api_search" : '{{ route("api_search") }}'
             }
         </script>
